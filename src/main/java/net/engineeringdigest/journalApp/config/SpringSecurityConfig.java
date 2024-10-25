d1aed67280305199f534496709740749188ff84f
utk145 <utkarshs14.2002@gmail.com>
2024-10-25 16:12:50 +0530

    basic usage profiles(to set up environment) in spring boot

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurityConfig.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import net.engineeringdigest.journalApp.types.UserRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Profile("dev")
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
