a3d406b18db2f9e5282628a17c3e2a060b058521
utk145 <utkarshs14.2002@gmail.com>
2024-10-27 11:20:51 +0530

    basic logger usage

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -3,12 +3,14 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JournalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JournalApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JournalApplication.class, args);
+        System.out.println(context.getEnvironment()); // to check the environment
     }
 
 }
--- src/main/java/net/engineeringdigest/journalApp/services/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/services/UserService.java
@@ -4,6 +4,8 @@ import net.engineeringdigest.journalApp.entity.UserEntity;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import net.engineeringdigest.journalApp.types.UserRoles;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,15 +22,26 @@ public class UserService {
 
     private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class); // static so that only single instance is created for complete  JournalEntryService class
+
     public void saveEntry(UserEntity user) {
         userRepository.save(user);
     }
 
     public void saveNewUser(UserEntity user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        user.setRoles(Arrays.asList(UserRoles.USER));
-        userRepository.save(user);
+        try {
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+            user.setRoles(Arrays.asList(UserRoles.USER));
+            userRepository.save(user);
+        } catch (Exception e) {
+//            logger.error("Error occurred: ", e);
+            logger.error("Error occurred for {}: with userId as {}  ", user.getUserName(), user.getUserId(), e.getMessage());
+//            logger.warn(e.toString());
+//            logger.info(e.toString());
+//            logger.debug(e.toString());
+//            logger.trace(e.toString());
+        }
     }
 
     public List<UserEntity> getAll() {
